9d0a6076f4ae23ee41477a2b85ceced55b4a76c0
ColdDraxal <darpanbasyal4@gmail.com>
2026-03-13 21:13:52 +0545

    updated

---
+++ oops1.py
@@ -0,0 +1,17 @@
+class Students:
+    name="Unknown"
+    age="Unknown"
+    def show_details(self):
+        print(f"Name : {self.name}")
+        print(f"Age : {self.age}")
+stud1=Students()
+stud2=Students()
+
+stud1.name="Darpan Basyal"
+stud1.age=21
+
+stud2.name="Brody Kaluwa"
+stud2.age=35
+
+stud1.show_details()
+stud2.show_details()
